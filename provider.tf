31696c4aa8b78b0bac6997bb7aeb3226c231c27b
CharanSai8127 <163974930+CharanSai8127@users.noreply.github.com>
2024-07-16 14:32:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,4 +3,16 @@ provider "aws" {
   region  = ap-south-1
 }
 
+# configure backend
+terraform {
+  backend "s3" {
+    bucket         = "devops-shack-s3"
+    key            = "eks.terraform.tfstate"
+    region         = "ap-south-1"
+
+  }
+}
+
+
+
 
